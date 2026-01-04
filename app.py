5c232044690034f9351f92389cd00af947a3d0f0
yfrtyftyfttf <145453788+yfrtyftyfttf@users.noreply.github.com>
2026-01-04 03:17:03 +0300

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,56 @@ app = Flask(__name__)
 
 # --- [ قسم البروكسيات ] ---
 RAW_PROXIES = """
-ضع البروكسيات هنا (كل واحد في سطر)
+82.24.249.101:5938:njhuvsdz:wp92l0dkdkoc 
+82.29.244.57:5880:njhuvsdz:wp92l0dkdkoc 
+198.89.123.107:6649:njhuvsdz:wp92l0dkdkoc 
+206.206.71.75:5715:njhuvsdz:wp92l0dkdkoc 
+23.27.210.135:6505:njhuvsdz:wp92l0dkdkoc 
+64.137.49.9:6550:njhuvsdz:wp92l0dkdkoc 
+66.63.180.174:5698:njhuvsdz:wp92l0dkdkoc 
+149.57.17.176:5644:njhuvsdz:wp92l0dkdkoc 
+154.6.59.162:6630:njhuvsdz:wp92l0dkdkoc 
+91.211.87.224:7214:njhuvsdz:wp92l0dkdkoc 
+199.180.8.177:5888:njhuvsdz:wp92l0dkdkoc 
+82.24.247.128:6962:njhuvsdz:wp92l0dkdkoc 
+198.105.119.196:5445:njhuvsdz:wp92l0dkdkoc 
+82.21.244.100:5423:njhuvsdz:wp92l0dkdkoc 
+82.25.213.249:5601:njhuvsdz:wp92l0dkdkoc 
+31.59.27.107:6684:njhuvsdz:wp92l0dkdkoc 
+45.43.87.128:7877:njhuvsdz:wp92l0dkdkoc 
+64.137.89.162:6235:njhuvsdz:wp92l0dkdkoc 
+142.147.245.203:5894:njhuvsdz:wp92l0dkdkoc 
+23.229.125.169:5438:njhuvsdz:wp92l0dkdkoc 
+31.223.189.234:6500:njhuvsdz:wp92l0dkdkoc 
+45.41.171.41:6077:njhuvsdz:wp92l0dkdkoc 
+205.164.57.143:5718:njhuvsdz:wp92l0dkdkoc 
+82.23.222.10:6316:njhuvsdz:wp92l0dkdkoc 
+209.242.203.117:6832:njhuvsdz:wp92l0dkdkoc 
+64.137.8.175:6857:njhuvsdz:wp92l0dkdkoc 
+23.27.138.82:6183:njhuvsdz:wp92l0dkdkoc 
+142.147.244.113:6357:njhuvsdz:wp92l0dkdkoc 
+82.23.222.38:6344:njhuvsdz:wp92l0dkdkoc 
+23.27.208.117:5827:njhuvsdz:wp92l0dkdkoc 
+31.59.33.141:6717:njhuvsdz:wp92l0dkdkoc 
+142.202.254.57:6035:njhuvsdz:wp92l0dkdkoc 
+82.22.235.92:6898:njhuvsdz:wp92l0dkdkoc 
+195.40.187.2:5184:njhuvsdz:wp92l0dkdkoc 
+66.63.180.153:5677:njhuvsdz:wp92l0dkdkoc 
+82.29.249.54:7891:njhuvsdz:wp92l0dkdkoc 
+108.165.227.16:5257:njhuvsdz:wp92l0dkdkoc 
+82.24.217.12:5342:njhuvsdz:wp92l0dkdkoc 
+45.131.92.195:6806:njhuvsdz:wp92l0dkdkoc 
+104.245.244.5:6445:njhuvsdz:wp92l0dkdkoc 
+145.223.47.135:6717:njhuvsdz:wp92l0dkdkoc 
+64.137.99.193:5826:njhuvsdz:wp92l0dkdkoc 
+204.217.161.171:6743:njhuvsdz:wp92l0dkdkoc 
+107.181.148.69:5929:njhuvsdz:wp92l0dkdkoc 
+173.211.30.43:6477:njhuvsdz:wp92l0dkdkoc 
+181.214.13.96:5937:njhuvsdz:wp92l0dkdkoc 
+104.252.41.95:7032:njhuvsdz:wp92l0dkdkoc 
+45.41.171.35:6071:njhuvsdz:wp92l0dkdkoc 
+46.203.154.160:5603:njhuvsdz:wp92l0dkdkoc 
+82.23.204.254:7086:njhuvsdz:wp92l0dkdkoc
 """
 PROXIES_LIST = [p.strip() for p in RAW_PROXIES.strip().split('\n') if p.strip()]
 
